ce35b2166a448394c8b36952cbd851a3e0436ef2
Frazer Joe Dourado <frazer.dourado@dcsa.org>
2021-09-28 08:54:21 +0200

    refactored package names.

--- src/main/java/org/dcsa/bkg/config/ApplicationConfig.java
+++ src/main/java/org/dcsa/bkg/config/ApplicationConfig.java
@@ -1,4 +1,4 @@
-package org.dcsa.booking.config;
+package org.dcsa.bkg.config;
 
 import org.dcsa.core.repository.ExtendedRepositoryImpl;
 import org.springframework.context.annotation.ComponentScan;
