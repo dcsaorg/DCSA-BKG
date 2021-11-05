bcce324c3521972d0e2cb1a5f47575e3ab537ec3
Martin Petersson <mp@asseco.dk>
2021-11-05 12:42:32 +0100

    Update BookingServiceImpl.java

--- src/main/java/org/dcsa/bkg/service/impl/BookingServiceImpl.java
+++ src/main/java/org/dcsa/bkg/service/impl/BookingServiceImpl.java
@@ -1,21 +1,15 @@
 package org.dcsa.bkg.service.impl;
 
 import org.dcsa.bkg.model.transferobjects.BookingConfirmationTO;
-import org.dcsa.bkg.model.transferobjects.BookingTO;
 import org.dcsa.bkg.model.transferobjects.BookingSummaryTO;
+import org.dcsa.bkg.model.transferobjects.BookingTO;
 import org.dcsa.bkg.service.BookingService;
-import org.dcsa.core.service.impl.BaseServiceImpl;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.UUID;
-
 @Service
-public class BookingServiceImpl extends BaseServiceImpl<BookingSummaryTO, UUID> implements BookingService {
+public class BookingServiceImpl implements BookingService {
 
   @Override
   public Flux<BookingSummaryTO> getBookingRequestSummaries() {
@@ -45,46 +39,5 @@ public class BookingServiceImpl extends BaseServiceImpl<BookingSummaryTO, UUID> 
   @Override
   public Mono<Void> cancelBookingByCarrierBookingReference(String carrierBookingReference) {
     return Mono.empty();
-  }
-
-  @Override
-  @ResponseStatus(HttpStatus.FORBIDDEN)
-  public Flux<BookingSummaryTO> findAll() {
-    return Flux.error(new ResponseStatusException(HttpStatus.FORBIDDEN));
-  }
-
-  @Override
-  @ResponseStatus(HttpStatus.FORBIDDEN)
-  public Mono<BookingSummaryTO> findById(UUID id) {
-    return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN));
-  }
-
-  @Override
-  @ResponseStatus(HttpStatus.FORBIDDEN)
-  public Mono<BookingSummaryTO> create(BookingSummaryTO bookingSummaryTO) {
-    return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN));
-  }
-
-  @Override
-  @ResponseStatus(HttpStatus.FORBIDDEN)
-  public Mono<BookingSummaryTO> update(BookingSummaryTO bookingSummaryTO) {
-    return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN));
-  }
-
-  @Override
-  @ResponseStatus(HttpStatus.FORBIDDEN)
-  public Mono<Void> deleteById(UUID id) {
-    return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN));
-  }
-
-  @Override
-  @ResponseStatus(HttpStatus.FORBIDDEN)
-  public Mono<Void> delete(BookingSummaryTO bookingSummaryTO) {
-    return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN));
-  }
-
-  @Override
-  public UUID getIdOfEntity(BookingSummaryTO entity) {
-    return null;
   }
 }
