15a954b8cc240152c423838b8904c44fab34d4ce
Martin Petersson <mp@asseco.dk>
2021-11-25 08:57:18 +0100

    Update ShipmentCutOffTimeTO.java

--- src/main/java/org/dcsa/bkg/model/transferobjects/ShipmentCutOffTimeTO.java
+++ src/main/java/org/dcsa/bkg/model/transferobjects/ShipmentCutOffTimeTO.java
@@ -1,14 +1,11 @@
 package org.dcsa.bkg.model.transferobjects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.dcsa.core.events.model.enums.CutOffDateTimeCode;
-import org.springframework.data.annotation.Id;
 
 import javax.validation.constraints.NotNull;
 import java.time.OffsetDateTime;
-import java.util.UUID;
 
 @Data
 public class ShipmentCutOffTimeTO {
