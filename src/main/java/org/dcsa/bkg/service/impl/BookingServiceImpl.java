02da88d483a277dce15263a1a0d29969fdef9f7b
pimg <pim.gaemers@dcsa.org>
2021-12-16 12:44:13 +0100

    Rebased dev re-added documentstatus and carrierbookingrequestreference to bookingsummary

--- src/main/java/org/dcsa/bkg/service/impl/BookingServiceImpl.java
+++ src/main/java/org/dcsa/bkg/service/impl/BookingServiceImpl.java
@@ -80,42 +80,46 @@ public class BookingServiceImpl implements BookingService {
 
   @Override
   public Flux<ShipmentSummaryTO> getShipmentSummaries(
-      String carrierBookingReference, DocumentStatus documentStatus, Pageable pageable) {
+    String carrierBookingReference, DocumentStatus documentStatus, Pageable pageable) {
 
     Flux<Shipment> shipmentResponse =
-        shipmentRepository.findAllByCarrierBookingReference(carrierBookingReference, pageable);
+      shipmentRepository.findAllByCarrierBookingReference(carrierBookingReference, pageable);
 
     if (carrierBookingReference == null) {
       shipmentResponse = shipmentRepository.findShipmentsByBookingIDNotNull(pageable);
     }
 
     return shipmentResponse.flatMap(
-        shipment -> {
-          // IF-statement is here so that we *only* make multiple queries if documentStatus is
-          // included as parameter
-          if (documentStatus != null) {
+      shipment -> {
+        // IF-statement is here so that we *only* make multiple queries if documentStatus is
+        // included as parameter
+        if (documentStatus != null) {
 
-            // Potential issue if booking query returns more than the set limit of results
-            return bookingRepository
-                .findAllByBookingIDAndDocumentStatus(
-                    shipment.getBookingID(), documentStatus, pageable)
-                .mapNotNull(
-                    ignored -> {
-                      ShipmentSummaryTO shipmentSummaryTO = new ShipmentSummaryTO();
-                      shipmentSummaryTO.setCarrierBookingReference(
-                          shipment.getCarrierBookingReference());
-                      shipmentSummaryTO.setConfirmationDateTime(shipment.getConfirmationDateTime());
-                      shipmentSummaryTO.setTermsAndConditions(shipment.getTermsAndConditions());
-                      return shipmentSummaryTO;
-                    });
-          } else {
-            ShipmentSummaryTO shipmentSummaryTO = new ShipmentSummaryTO();
-            shipmentSummaryTO.setCarrierBookingReference(shipment.getCarrierBookingReference());
-            shipmentSummaryTO.setConfirmationDateTime(shipment.getConfirmationDateTime());
-            shipmentSummaryTO.setTermsAndConditions(shipment.getTermsAndConditions());
-            return Mono.just(shipmentSummaryTO);
-          }
-        });
+          // Potential issue if booking query returns more than the set limit of results
+          return bookingRepository
+            .findAllByBookingIDAndDocumentStatus(
+              shipment.getBookingID(), documentStatus, pageable)
+            .mapNotNull(
+              booking -> createShipmentSummaryTO(shipment, booking));
+        } else {
+          return bookingRepository.findById(shipment.getBookingID()).mapNotNull(
+            booking -> createShipmentSummaryTO(shipment, booking)
+          );
+        }
+      });
+  }
+
+  private ShipmentSummaryTO createShipmentSummaryTO(Shipment shipment, Booking booking) {
+    ShipmentSummaryTO shipmentSummaryTO =
+      new ShipmentSummaryTO();
+    shipmentSummaryTO.setCarrierBookingReference(
+      shipment.getCarrierBookingReference());
+    shipmentSummaryTO.setConfirmationDateTime(
+      shipment.getConfirmationDateTime());
+    shipmentSummaryTO.setTermsAndConditions(shipment.getTermsAndConditions());
+    shipmentSummaryTO.setCarrierBookingRequestReference(booking.getCarrierBookingRequestReference());
+    shipmentSummaryTO.setDocumentStatus(booking.getDocumentStatus());
+    return shipmentSummaryTO;
   }
 
   @Override
