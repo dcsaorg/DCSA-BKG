f808f20fb0d4157da86f7a19cdbdf6733576b9c9
pimg <pim.gaemers@dcsa.org>
2021-12-16 12:44:13 +0100

    Added documentstatus and carrierbookingrequestreference to bookingsummary

--- src/main/java/org/dcsa/bkg/model/transferobjects/ShipmentSummaryTO.java
+++ src/main/java/org/dcsa/bkg/model/transferobjects/ShipmentSummaryTO.java
@@ -1,8 +1,9 @@
 package org.dcsa.bkg.model.transferobjects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import org.springframework.data.relational.core.mapping.Column;
+import org.dcsa.core.events.model.enums.DocumentStatus;
 
 import javax.validation.constraints.Size;
 import java.time.OffsetDateTime;
@@ -11,13 +12,19 @@ import java.time.OffsetDateTime;
 public class ShipmentSummaryTO {
 
   @Size(max = 35)
-  @Column("carrier_booking_reference")
+//  @Column("carrier_booking_reference")
   private String carrierBookingReference;
 
-  @Column("terms_and_conditions")
+//  @Column("terms_and_conditions")
   private String termsAndConditions;
 
   @JsonFormat(shape = JsonFormat.Shape.STRING)
-  @Column("confirmation_datetime")
+//  @Column("confirmation_datetime")
   private OffsetDateTime confirmationDateTime;
+
+  @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+  private String carrierBookingRequestReference;
+
+  @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+  private DocumentStatus documentStatus;
 }
