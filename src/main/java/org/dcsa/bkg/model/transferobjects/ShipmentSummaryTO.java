6639aa2b5ea691489cccf3d3a39120b0b07c68a9
pimg <pim.gaemers@dcsa.org>
2021-12-17 11:28:17 +0100

    Cleaned up ShipmentSummaryTO removed entity related annotation comments

--- src/main/java/org/dcsa/bkg/model/transferobjects/ShipmentSummaryTO.java
+++ src/main/java/org/dcsa/bkg/model/transferobjects/ShipmentSummaryTO.java
@@ -12,14 +12,11 @@ import java.time.OffsetDateTime;
 public class ShipmentSummaryTO {
 
   @Size(max = 35)
-//  @Column("carrier_booking_reference")
   private String carrierBookingReference;
 
-//  @Column("terms_and_conditions")
   private String termsAndConditions;
 
   @JsonFormat(shape = JsonFormat.Shape.STRING)
-//  @Column("confirmation_datetime")
   private OffsetDateTime confirmationDateTime;
 
   @JsonProperty(access = JsonProperty.Access.READ_ONLY)
