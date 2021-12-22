f715ca42900b0baa012101fc646252e76ca8ce4a
pimg <pim.gaemers@dcsa.org>
2021-12-22 16:12:15 +0100

    Removed annotation parameter in BookingSummaryMapper

--- src/main/java/org/dcsa/bkg/model/mappers/BookingSummaryMapper.java
+++ src/main/java/org/dcsa/bkg/model/mappers/BookingSummaryMapper.java
@@ -4,12 +4,9 @@ package org.dcsa.bkg.model.mappers;
 import org.dcsa.bkg.model.transferobjects.BookingSummaryTO;
 import org.dcsa.core.events.model.Booking;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface BookingSummaryMapper {
 
-
-    @Mapping(target = "expectedDepartureDate", ignore = true)
     BookingSummaryTO bookingSummaryTOFromBooking(Booking booking);
 }
